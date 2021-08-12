57550a2f8269bfcc62c08003437ded016f893a33
近藤岳瑠 <takechihorn@gmail.com>
2021-08-13 04:56:55 +0900

    first commit

---
+++ client/client.js
@@ -0,0 +1,133 @@
+var greets = require("../server/protos/greet_pb");
+var service = require("../server/protos/greet_grpc_pb");
+
+var calc = require("../server/protos/calculator_pb");
+var calcService = require("../server/protos/calculator_grpc_pb");
+var grpc = require("grpc");
+
+function callGreetings() {
+  console.log("Hello From Client");
+
+  // Created our server client
+  var client = new service.GreetServiceClient(
+    "localhost:50051",
+    grpc.credentials.createInsecure()
+  );
+
+  //  create our server request
+  var request = new greets.GreetRequest();
+
+  // created a protocol buffer greeting message
+  var greeting = new greets.Greeting();
+  greeting.setFirstName("Takeru");
+  greeting.setLastName("Kondoooooooooooooo");
+
+  // set the Greeting
+  request.setGreeting(greeting);
+  client.greet(request, (error, response) => {
+    if (!error) {
+      console.log("Greeting Response: ", response.getResult());
+    } else {
+      console.error(error);
+    }
+  });
+
+  console.log("client", client);
+}
+
+// ターミナルに表示させる処理
+function callSum() {
+  // Created our server client
+  var client = new calcService.CalculatorServiceClient(
+    "localhost:50051",
+    grpc.credentials.createInsecure()
+  );
+
+  // リクエストを作る
+  var sumRequest = new calc.SumRequest();
+  sumRequest.setFirstNumber(10);
+  sumRequest.setSecondNumber(15);
+
+  client.sum(sumRequest, (error, response) => {
+    if (!error) {
+      console.log(
+        sumRequest.getFirstNumber() +
+          " + " +
+          sumRequest.getSecondNumber() +
+          " = " +
+          response.getSumResult()
+      );
+    } else {
+      console.error(error);
+    }
+  });
+
+  console.log("client", client);
+}
+
+function callGreetManyTimes() {
+  var client = new service.GreetServiceClient(
+    "localhost:50051",
+    grpc.credentials.createInsecure()
+  );
+
+  // create request
+  var request = new greets.GreetManyTimesRequest();
+
+  var greeting = new greets.Greeting();
+  greeting.setFirstName("Takeru");
+  greeting.setLastName("Kondoooooooooooooo");
+  request.setGreeting(greeting);
+
+  var call = client.greetManyTimes(request, () => {});
+
+  call.on("data", (response) => {
+    console.log("Client Streaming Response: ", response.getResult());
+  });
+  call.on("status", (status) => {
+    console.log(status.details);
+  });
+
+  call.on("error", (error) => {
+    console.error(error.details);
+  });
+
+  call.on("end", () => {
+    console.log("Streaming Ended!");
+  });
+}
+
+function callPrimeNumberDecomposition() {
+  var client = new calcService.CalculatorServiceClient(
+    "localhost:50051",
+    grpc.credentials.createInsecure()
+  );
+  var request = new calc.PrimeNumberDecompositionRequest();
+
+  var number = 7897896;
+  request.setNumber(number);
+
+  var call = client.primeNumberDecomposition(request, () => {});
+
+  call.on("data", (response) => {
+    console.log("Prime Factors Found: ", response.getPrimeFactor());
+  });
+  call.on("error", (error) => {
+    console.error(error);
+  });
+  call.on("status", (status) => {
+    console.log(status);
+  });
+
+  call.on("end", () => {
+    console.log("Streaming Ended!");
+  });
+}
+
+function main() {
+  callPrimeNumberDecomposition();
+  // callGreetManyTimes();
+  //callGreetings();
+  // callSum();
+}
+main();
---
+++ server/index.js
@@ -0,0 +1,91 @@
+var greets = require("../server/protos/greet_pb");
+var service = require("../server/protos/greet_grpc_pb");
+
+var calc = require("../server/protos/calculator_pb");
+var calcService = require("../server/protos/calculator_grpc_pb");
+
+var grpc = require("grpc");
+
+/*
+  Implements the greet RPC method.
+*/
+// Server側で計算をする
+function sum(call, callback) {
+  var sumResponse = new calc.SumResponse();
+  sumResponse.setSumResult(
+    call.request.getFirstNumber() + call.request.getSecondNumber()
+  );
+  // 返す処理callbackにやらせる
+  callback(null, sumResponse);
+}
+
+function greetManyTimes(call, callback) {
+  var firstName = call.request.getGreeting().getFirstName();
+
+  let count = 0,
+    intervalID = setInterval(function () {
+      var greetManyTimesResponse = new greets.GreetManyTimesResponse();
+      greetManyTimesResponse.setResult(firstName);
+
+      // setup streaming
+      call.write(greetManyTimesResponse);
+
+      if (++count > 9) {
+        clearInterval(intervalID);
+        call.end(); // we have sent all messages!
+      }
+    }, 1000);
+}
+
+// primeFactor -
+function primeNumberDecomposition(call, callback) {
+  var number = call.request.getNumber();
+  var divisor = 2;
+
+  console.log("Received number: ", number);
+  while (number > 1) {
+    if (number % divisor === 0) {
+      var primeNumberDecompositionResponse =
+        new calc.PrimeNumberDecompositionResponse();
+      primeNumberDecompositionResponse.setPrimeFactor(divisor);
+
+      number = number / divisor;
+      //write the message using call.write()
+
+      call.write(primeNumberDecompositionResponse);
+    } else {
+      divisor++;
+      console.log("Divisor has increased to ", divisor);
+    }
+  }
+  call.end(); // all messages sent! we are done
+}
+
+function greet(call, callback) {
+  var greeting = new greets.GreetResponse();
+
+  greeting.setResult(
+    "Hello " +
+      call.request.getGreeting().getFirstName() +
+      " " +
+      call.request.getGreeting().getLastName()
+  );
+  callback(null, greeting);
+}
+
+function main() {
+  var server = new grpc.Server();
+  server.addService(calcService.CalculatorServiceService, {
+    sum: sum,
+    primeNumberDecomposition: primeNumberDecomposition,
+  });
+  // server.addService(service.GreetServiceService, {
+  //   greet: greet,
+  //   greetManyTimes: greetManyTimes,
+  // });
+  server.bind("127.0.0.1:50051", grpc.ServerCredentials.createInsecure());
+  server.start();
+  console.log("Server running on port 127.0.0.1:50051");
+}
+
+main();
